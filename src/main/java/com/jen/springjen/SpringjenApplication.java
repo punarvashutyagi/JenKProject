6cb9d0ae64d2ff81677a6da804a3f8b2e2a76b61
student <punarvashutyagi@gmail.com>
2021-03-27 17:17:18 +0000

    first jenkinsfile

---
+++ src/main/java/com/jen/springjen/SpringjenApplication.java
@@ -0,0 +1,13 @@
+package com.jen.springjen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringjenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jen/springjen/SpringjenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jen.springjen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
